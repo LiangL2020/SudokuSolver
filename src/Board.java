1219050c9a64a558ded9e988541e0af9862ae1cb
liangl2020 <liang_lu@student.wayland.k12.ma.us>
2018-09-11 09:15:03 -0400

    isValid deleted lol I figured that wasn't the right spot

--- src/Board.java
+++ src/Board.java
@@ -8,14 +8,4 @@ public class Board {
 
     }
 
-    //check if it is in the box
-    public boolean isValid(){
-        for (int r = 0; r < boxes.length; r++) {
-            for (int c = 0; c < boxes[0].length; c++) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }

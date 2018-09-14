f0956e4aaae6994c5398b6634fa9d4eb7cd23f0d
liangl2020 <liang_lu@student.wayland.k12.ma.us>
2018-09-14 09:36:45 -0400

    two sets of fori loop added in box class

--- src/Box.java
+++ src/Box.java
@@ -2,9 +2,24 @@ public class Box {
 
     private Unit[][] units = new Unit[3][3];
 
-    public Box(Unit[][] units){
+    public Box(Unit[][] units) {
 
         this.units = units;
+
+//if there is a value || only 1 posval        if(){
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                //make 9 units in each box
+                //units[r][c] = getValue();
+            }
+        }
+//if ends        }//else if more than 1 posval{
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                //this inserts posval
+            }
+        }
+//else ends    }
 
     }
 

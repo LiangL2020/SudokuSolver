30121dfc73d0d808506c98e5cb2828450a7e95fa
liangl2020 <liang_lu@student.wayland.k12.ma.us>
2018-09-21 09:38:16 -0400

    checking if the rows have numbers 1-9
    working on rows and boxex

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Board {
 
     private Box[][] boxes = new Box[3][3];
@@ -77,6 +79,41 @@ public class Board {
         System.out.println();
         System.out.println();
 
+    }
+
+    //checks if the numbers are 1-9
+    public boolean checkRow(int row){
+        int[] check = new int[9]; //counter...check[0] counts the number of 1's in row, [1] counts 2's....
+
+        for (int j = 0; j < check.length; j++) {
+            String value = boxes[row/3][j/3].getUnits()[row%3][j%3].getValue();
+
+            int num = Integer.parseInt(value);
+            check[num-1]++;
+        }
+
+        for(int n : check)
+            if(n > 1)
+                return false;
+        return true;
+    }
+
+    //TODO: finish
+    public boolean checkCol(int col){
+        int[] check = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            String value = boxes[i/3][col/3].getUnits()[i%3][col%3].getValue();
+
+            int num = Integer.parseInt(value);
+            check[num-1]++;
+        }
+
+        for(int n : check){
+            if(n > 1)
+                return false;
+        }
+        return true;
     }
 
 }
